d40ee103f0348519f7328d84b3762397f068ad1d
JP Reddy <guthijp.reddy@gmail.com>
2018-03-02 17:59:20 +0530

    https://www.hackerrank.com/challenges/minimum-average-waiting-time/problem

---
+++ avg_waiting_time_heaps.py
@@ -0,0 +1,31 @@
+# https://www.hackerrank.com/challenges/minimum-average-waiting-time/problem
+
+# Nice Solution 
+from heapq import heappush, heappop
+
+tasks = []
+
+N = int(input())
+
+for _ in range(N):
+    arrival, cook_time = map(int, input().split())
+    tasks.append((arrival, cook_time))
+
+tasks.sort(reverse=True)
+# print(tasks)
+pq = []
+time_waiting = 0
+current_time = 0
+
+while tasks or pq:
+    while tasks and tasks[-1][0] <= current_time:
+        heappush(pq, tasks.pop()[::-1])
+    if pq:
+        current_task = heappop(pq)
+        current_time += current_task[0]
+        time_waiting += current_time - current_task[1]
+    else:
+        heappush(pq, tasks.pop()[::-1])
+        current_time = pq[0][1]
+        
+print(time_waiting // N)
