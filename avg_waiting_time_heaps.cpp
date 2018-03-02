5e4fc501cacfd526880ceb452cafedf8eecef652
JP Reddy <guthijp.reddy@gmail.com>
2018-03-02 18:10:11 +0530

    https://www.hackerrank.com/challenges/minimum-average-waiting-time/problem

--- avg_waiting_time_heaps.cpp
+++ avg_waiting_time_heaps.cpp
@@ -8,7 +8,16 @@ class CompareDist
 {
 public:
     bool operator()(pair<long int,long int> n1,pair<long int,long int> n2) {
-        return n1.second>n2.second;
+        if(n1.second!=n2.second)
+            return n1.second>n2.second;
+        if(n1.first>=n2.first)
+        {
+        
+        return n1.first>n2.first;
+            
+        }
+        else
+            return n2.first>n1.first;
     }
 };
 
@@ -28,8 +37,6 @@ int main()
 		tasks.push_back(make_pair(arrival,cook_time));
 	}
 	sort(tasks.begin(),tasks.end());
-	// for(i=0;i<tasks.size();i++)
-	// cout<<tasks[i].first<<"\t"<<tasks[i].second<<endl;
 	while(!tasks.empty() || !minheap.empty())
 	{
 		while(!tasks.empty() && tasks[0].first<=cur_time)
@@ -40,7 +47,7 @@ int main()
 		if(!minheap.empty())
 		{
 			cur_task=minheap.top();
-	minheap.pop();
+            minheap.pop();
 			cur_time+=cur_task.second;
 			wait_time+=cur_time-cur_task.first;
 		}
@@ -50,17 +57,6 @@ int main()
 			tasks.erase(tasks.begin());
 			cur_time=minheap.top().first;
 		}
-	// cout<<"Current time: "<<cur_time<<endl;
-	// cout<<"Task contents are: \n";
-	// for(i=0;i<tasks.size();i++)
-	// cout<<tasks[i].first<<"\t"<<tasks[i].second<<endl;
-	// cout<<"Heap contents are: \n";
-	// copy=minheap;
-	// while(!copy.empty())
-	// {
-	// cout<<copy.top().second<<"\t"<<copy.top().first<<endl;
-	// copy.pop();
-	// }
 	}
 	cout<<(wait_time/n);
 }
