7619b400644c6629e535c1ec269c96e46ea172df
JP-Reddy <guthijp.reddy@gmail.com>
2018-07-18 22:42:34 +0530

    https://leetcode.com/problems/balanced-binary-tree/description/

---
+++ balanced_binary_tree2.cpp
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/balanced-binary-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int height(TreeNode *root){
+        if(root==NULL)
+            return 0;
+        int lh=height(root->left);
+        if(lh==-1)
+            return -1;
+        int rh=height(root->right);
+        if(rh==-1)
+            return -1;
+        if(abs(lh-rh)>1)
+            return -1;
+        return max(lh,rh)+1;
+    }
+    
+    bool isBalanced(TreeNode* root) {
+        if(root==NULL)
+            return true;
+        return height(root)!=-1;
+    }
+};
