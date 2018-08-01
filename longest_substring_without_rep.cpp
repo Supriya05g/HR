2a9efc5e7ea1214f4cad4fea281aff358cd14c4f
JP-Reddy <guthijp.reddy@gmail.com>
2018-08-01 21:39:57 +0530

    // https://leetcode.com/problems/longest-substring-without-repeating-characters/description/

---
+++ longest_substring_without_rep.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> dict(256,-1);
+        int maxlen=0,start=-1;
+        for(int i=0;i<s.length();i++){
+            if(dict[s[i]]>start)
+                start=dict[s[i]];
+            dict[s[i]]=i;
+            maxlen=max(maxlen,i-start);
+        }
+        return maxlen;
+        
+        }
+};
