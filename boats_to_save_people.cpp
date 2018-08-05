82de27069361c9a7e8fd5e5e66d79dc3f946aba6
JP-Reddy <guthijp.reddy@gmail.com>
2018-08-05 09:47:14 +0530

    https://leetcode.com/problems/boats-to-save-people/description/

---
+++ boats_to_save_people.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/boats-to-save-people/description/
+
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int boats=0;
+        for(int i=0,j=people.size()-1;i<=j;++boats,--j)
+            if(people[i]+people[j]<=limit) i++;
+    return boats;
+    }
+};
